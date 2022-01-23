e155839bd392250c9cdb4093d68b0393bc028060
Kenneth Trinh <longtrinh28@gmal.com>
2022-01-22 19:59:27 -0500

    optimized Gradient Heap

--- utils.py
+++ utils.py
@@ -85,48 +85,59 @@ class PhaseVocoder:
         self.window_size = window_size
         self.synthesis_hopsize = window_size//4
         self.analysis_hopsize = int(self.synthesis_hopsize//pitch_ratio)
-        self.last_phase = np.zeros(window_size//2+1, dtype=np.float32) #frame n − 1
-        self.accum_phase = np.zeros(window_size//2+1, dtype=np.float32)
-        self.expected_phase = np.linspace(0, window_size//2, window_size//2+1)*2*np.pi*self.analysis_hopsize//window_size  # expected phase (2049,)
-        self.last_magnitudes = np.zeros(window_size//2+1, dtype=np.float32) #frame n − 1
+        self.alpha = self.synthesis_hopsize/self.analysis_hopsize
+        self.HALF_FFT = window_size//2+1
+        self.last_phase = np.zeros(self.HALF_FFT) #frame n − 1
+        self.accum_phase = np.zeros(self.HALF_FFT)
+        self.last_accum_phase = np.zeros(self.HALF_FFT)
+        self.expected_phase = np.linspace(0, window_size//2, self.HALF_FFT)*2*np.pi*self.analysis_hopsize//window_size  # expected phase (2049,)
+        self.last_magnitudes = np.zeros(self.HALF_FFT) #frame n − 1
     def calc_phase(self, current_frame):
         """Saves previous phase values for calculation of next frame"""
         current_phase = np.angle(current_frame)
         current_magn = abs(current_frame)
         delta_phase = np.unwrap(current_phase - self.last_phase - self.expected_phase)
         phase_derivative = self.expected_phase + delta_phase
-        frequency_derivative = np.append(self.last_phase[1:] - self.last_phase[-1], 0)
+        frequency_derivative = np.unwrap( np.r_[0, (current_phase[1:] - current_phase[:-1])] )
         #self.accum_phase += phase_derivative*(self.synthesis_hopsize/self.analysis_hopsize) #phi(k, n+1)
         self.Gradient_Heap(current_magn, phase_derivative, frequency_derivative)
-        self.last_phase = current_phase.copy()
-        self.last_magnitudes = current_magn.copy()
+        self.last_accum_phase = self.accum_phase.copy()
+        self.last_phase = current_phase
+        self.last_magnitudes = current_magn
         return current_magn*np.exp(1j*self.accum_phase)
     def update(self, pitch_ratio):
         """ Called when pitch is changed """
         self.analysis_hopsize = int(self.synthesis_hopsize//pitch_ratio)
-        self.expected_phase = np.linspace(0, self.window_size//2, self.window_size//2+1)*2*np.pi*self.analysis_hopsize//self.window_size
-    def Gradient_Heap(self, current_magn, phase_derivative, frequency_derivative, threshold=0.005):
+        self.expected_phase = np.linspace(0, self.window_size//2, self.HALF_FFT)*2*np.pi*self.analysis_hopsize//self.window_size
+        self.alpha = self.synthesis_hopsize/self.analysis_hopsize
+    def Gradient_Heap(self, current_magn, phase_derivative, frequency_derivative, threshold=0.5):
         """Implementation gradient propagation algorithm - Makes all magnitudes negative for Max heap """
-        threshold = threshold* max( current_magn.max(), self.last_magnitudes.max() )
-        print(threshold)
-        previous_indices = np.where( self.last_magnitudes > threshold )[0]
-        heap = list( zip(self.last_magnitudes[previous_indices] * -1, previous_indices, np.zeros(previous_indices.shape[0]) ) )
-        heapq.heapify(heap) # initialize with all elements from frame n - 1
-        current_indices = np.where( current_magn > threshold )[0]
-        I = set( zip( current_indices, np.ones(current_indices.shape[0]) ) ) #visited
+        #threshold = threshold* max( current_magn.max(), self.last_magnitudes.max() )
+        heap = []
+        I = set()
+        for i in range(self.HALF_FFT):
+            if self.last_magnitudes[i] > threshold:
+                heapq.heappush(heap, (-self.last_magnitudes[i], i, 0) )
+            if current_magn[i] > threshold:
+                I.add( (i, 1) )
+        # previous_indices = np.where( self.last_magnitudes > threshold )[0]
+        # heap = list( zip(self.last_magnitudes[previous_indices] * -1, previous_indices, np.zeros(previous_indices.shape[0]) ) )
+        # heapq.heapify(heap) # initialize with all elements from frame n - 1
+        # current_indices = np.where( current_magn > threshold )[0]
+        # I = set( zip( current_indices, np.ones(current_indices.shape[0]) ) ) #visited
         while I and heap:
             G, k, n = heapq.heappop(heap)
             if n == 0:
                 if (k, n+1) in I:
-                    self.accum_phase[k] = self.accum_phase[k] +  phase_derivative[k]*(self.synthesis_hopsize/self.analysis_hopsize)
+                    self.accum_phase[k] = self.last_accum_phase[k] + phase_derivative[k] * self.alpha
                     I.remove( (k, n+1) )
                     heapq.heappush( heap, (-current_magn[k], k , n+1) )
             if n == 1:
                 if (k+1, n) in I:
-                    self.accum_phase[k + 1] = self.accum_phase[k] + frequency_derivative[k + 1]*(self.synthesis_hopsize/self.analysis_hopsize)
+                    self.accum_phase[k + 1] = self.accum_phase[k] +  frequency_derivative[k + 1] * self.alpha
                     I.remove( (k+1, n) )
                     heapq.heappush( heap, (-current_magn[k+1], k+1 , n) )
                 if (k-1, n) in I:
-                    self.accum_phase[k - 1] = self.accum_phase[k] + frequency_derivative[k - 1]*(self.synthesis_hopsize/self.analysis_hopsize)
+                    self.accum_phase[k - 1] = self.accum_phase[k] +  frequency_derivative[k - 1] * self.alpha
                     I.remove( (k-1, n) )
                     heapq.heappush( heap, (-current_magn[k-1], k-1 , n) )
